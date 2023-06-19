5008a17e6083d7868034bc14b690d502dd26719f
Nils Leo <nilstleo01@gmail.com>
2023-06-20 01:17:55 +0200

    feat(authentication): add Google API call to validate access token and retrieve user information

--- remsfal-service/src/main/java/de/remsfal/service/boundary/authentication/TokenValidator.java
+++ remsfal-service/src/main/java/de/remsfal/service/boundary/authentication/TokenValidator.java
@@ -1,9 +1,14 @@
 package de.remsfal.service.boundary.authentication;
 
+import java.io.IOException;
+import java.net.URI;
 import javax.enterprise.context.ApplicationScoped;
-
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import de.remsfal.core.dto.ImmutableUserJson;
 import de.remsfal.core.model.UserModel;
+import org.json.JSONObject;
 
 /**
  * @author Alexander Stanik [stanik@htw-berlin.de]
@@ -12,12 +17,38 @@ import de.remsfal.core.model.UserModel;
 public class TokenValidator {
 
     public TokenInfo validate(final String authorizationHeader) {
-        final UserModel dummyUser = ImmutableUserJson.builder()
-            .id("8af38cd0-5aac-4d14-b3e6-341d0492836e")
-            .email("dummy@example.org")
-            .name("Max Mustermann")
-            .build();
-        return new TokenInfo(dummyUser);
-    }
+        String accessToken = authorizationHeader.replace("Bearer ", "");
+        String responseString = "";
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("https://www.googleapis.com/oauth2/v1/userinfo"))
+                    .header("Authorization", "Bearer " + accessToken)
+                    .GET()
+                    .build();
 
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            responseString = response.body();
+
+            System.out.println(response.statusCode());
+            System.out.println("Google API response: " + response.body());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        JSONObject responseJson = new JSONObject(responseString);
+        System.out.println("Google API response: " + responseJson.toString());
+        String id = responseJson.getString("id");
+        String userName = responseJson.getString("name");
+        String userEmail = responseJson.getString("email");
+
+        final UserModel user = ImmutableUserJson.builder()
+                .id(id)
+                .email(userEmail)
+                .name(userName)
+                .build();
+        return new TokenInfo(user);
+    }
 }
