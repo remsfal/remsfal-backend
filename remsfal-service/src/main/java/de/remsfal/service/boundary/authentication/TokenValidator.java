036943f78de8039e8d25cb238dd6afcfb8bd1484
Nils Leo <92734161+NilsLeo@users.noreply.github.com>
2023-07-17 07:57:47 +0200

    Added Google Authentication (#6)
    * docs(README.md): add instructions to start the database and backend server using docker and maven respectively
    
    * feat(authentication): add Google API call to validate access token and retrieve user information
    
    * chore(pom.xml): add dependencies for jjwt, google-api-client, google-http-client, and google-oauth-client to support authentication and API calls
    
    refactor(TokenValidator.java): update TokenValidator class to use GoogleIdTokenVerifier for token validation and extract user information from the token payload
    
    * fix(TokenValidator.java): change JacksonFactory to GsonFactory to use Gson for JSON processing
    
    The change was made to improve the JSON processing in the TokenValidator class. The JacksonFactory was replaced with the GsonFactory to use Gson library for JSON processing. This change ensures better compatibility and performance in handling JSON data.
    
    * chore(pom.xml): remove unused dependencies to declutter the project and improve build performance

--- remsfal-service/src/main/java/de/remsfal/service/boundary/authentication/TokenValidator.java
+++ remsfal-service/src/main/java/de/remsfal/service/boundary/authentication/TokenValidator.java
@@ -1,23 +1,49 @@
 package de.remsfal.service.boundary.authentication;
 
-import javax.enterprise.context.ApplicationScoped;
-
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
 import de.remsfal.core.dto.ImmutableUserJson;
 import de.remsfal.core.model.UserModel;
 
-/**
- * @author Alexander Stanik [stanik@htw-berlin.de]
- */
+import javax.enterprise.context.ApplicationScoped;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+
 @ApplicationScoped
 public class TokenValidator {
+    private static final String CLIENT_ID = "821093255871-m7fg18oh8je55vaknjur9pgrh8sh4atb.apps.googleusercontent.com";
 
     public TokenInfo validate(final String authorizationHeader) {
-        final UserModel dummyUser = ImmutableUserJson.builder()
-            .id("8af38cd0-5aac-4d14-b3e6-341d0492836e")
-            .email("dummy@example.org")
-            .name("Max Mustermann")
-            .build();
-        return new TokenInfo(dummyUser);
-    }
+        String idTokenString = authorizationHeader.replace("Bearer ", "");
 
+        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new GsonFactory())
+                .setAudience(Collections.singletonList(CLIENT_ID))
+                .build();
+
+        try {
+            GoogleIdToken idToken = verifier.verify(idTokenString);
+            if (idToken != null) {
+                GoogleIdToken.Payload payload = idToken.getPayload();
+
+                String userId = payload.getSubject();
+                String email = payload.getEmail();
+                String name = (String) payload.get("name");
+            System.out.println("payload " + payload);
+                final UserModel user = ImmutableUserJson.builder()
+                        .id(userId)
+                        .email(email)
+                        .name(name)
+                        .build();
+
+                return new TokenInfo(user);
+            }
+        } catch (GeneralSecurityException | IOException e) {
+            System.out.println("Invalid ID token.");
+        }
+
+        return null;
+    }
 }
