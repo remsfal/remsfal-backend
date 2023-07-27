17f681c9b32aa4f95ed859e571c57727751a8997
Nils Leo <nilstleo01@gmail.com>
2023-07-27 22:22:42 +0200

    save

--- remsfal-service/src/main/java/de/remsfal/service/boundary/authentication/TokenValidator.java
+++ remsfal-service/src/main/java/de/remsfal/service/boundary/authentication/TokenValidator.java
@@ -1,5 +1,7 @@
 package de.remsfal.service.boundary.authentication;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
 import com.google.api.client.http.javanet.NetHttpTransport;
@@ -9,14 +11,18 @@ import de.remsfal.core.model.UserModel;
 
 import javax.enterprise.context.ApplicationScoped;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.security.GeneralSecurityException;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Date;
 
 @ApplicationScoped
 public class TokenValidator {
     private static final String CLIENT_ID = "821093255871-m7fg18oh8je55vaknjur9pgrh8sh4atb.apps.googleusercontent.com";
 
     public TokenInfo validate(final String authorizationHeader) {
+        System.out.println("authHeader" + authorizationHeader);
         String idTokenString = authorizationHeader.replace("Bearer ", "");
 
         GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new GsonFactory())
@@ -25,18 +31,20 @@ public class TokenValidator {
 
         try {
             GoogleIdToken idToken = verifier.verify(idTokenString);
+
             if (idToken != null) {
                 GoogleIdToken.Payload payload = idToken.getPayload();
 
                 String userId = payload.getSubject();
                 String email = payload.getEmail();
                 String name = (String) payload.get("name");
-            System.out.println("payload " + payload);
                 final UserModel user = ImmutableUserJson.builder()
                         .id(userId)
                         .email(email)
                         .name(name)
                         .build();
+
+                System.out.println("uname" +user.getEmail());
 
                 return new TokenInfo(user);
             }
@@ -47,8 +55,6 @@ public class TokenValidator {
         return null;
     }
 
-    String generateJWT(String authorizationHeader){
-        String idTokenString = authorizationHeader.replace("Bearer ", "");
-        return "Bearer ey123456789";
-    }
+
+
 }
