6d189d18deab0e4696a6f03882540b07e50989a1
Nils Leo <nilstleo01@gmail.com>
2023-07-27 19:43:08 +0200

    added generate Keys script

--- remsfal-service/src/main/resources/generate_jwtKeys.sh
+++ remsfal-service/src/main/resources/generate_jwtKeys.sh
@@ -1,6 +1,12 @@
-mkdir jwt
-openssl genrsa -out rsaPrivateKey.pem 2048 -ii
+# Generate a 2048-bit RSA private key
+openssl genrsa -out rsaPrivateKey.pem 2048
+
+# Generate the corresponding public key
 openssl rsa -pubout -in rsaPrivateKey.pem -out publicKey.pem
+
+# Convert RSA private key to PKCS8 format
 openssl pkcs8 -topk8 -nocrypt -inform pem -in rsaPrivateKey.pem -outform pem -out privateKey.pem
+
+# Set the permissions so only the user can read and write the key files
 chmod 600 rsaPrivateKey.pem
 chmod 600 privateKey.pem
