e99b612b13f9d28089227d639d40f33e2431db5a
Alexey Leping <aleping@yandex.ru>
2024-08-12 11:05:23 +0300

    add new tests for createBuilding()

--- remsfal-service/src/main/java/de/remsfal/service/control/BuildingController.java
+++ remsfal-service/src/main/java/de/remsfal/service/control/BuildingController.java
@@ -3,12 +3,13 @@ package de.remsfal.service.control;
 import de.remsfal.core.json.BuildingJson;
 import de.remsfal.core.model.*;
 import de.remsfal.service.entity.dao.BuildingRepository;
-import de.remsfal.service.entity.dao.ProjectRepository;
-import de.remsfal.service.entity.dao.PropertyRepository;
 import de.remsfal.service.entity.dto.*;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
+import jakarta.persistence.NoResultException;
 import jakarta.transaction.Transactional;
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.Response;
 import org.jboss.logging.Logger;
 
 
@@ -21,27 +22,35 @@ public class BuildingController {
     BuildingRepository buildingRepository;
 
     public BuildingModel getBuilding(String projectId, String propertyId, String buildingId) {
-        return buildingRepository.findByProjectIdAndByPropertyIdAndByBuildingId(projectId, propertyId, buildingId);
+        try {
+            return buildingRepository.findByProjectIdAndByPropertyIdAndByBuildingId(projectId, propertyId, buildingId);
+        } catch (NoResultException e) {
+            throw new WebApplicationException("Building not found", Response.Status.NOT_FOUND);
+        }
     }
 
     @Transactional
     public BuildingModel createBuilding(String projectId, String building, BuildingJson property) {
         logger.infov("Creating a building (title={0}, address={1})", property.getTitle(), property.getAddress());
-        BuildingEntity buildingEntity = new BuildingEntity();
-        buildingEntity.setProjectId(projectId);
-        buildingEntity.generateId();
-        buildingEntity.setPropertyId(building);
-        buildingEntity.setTitle(property.getTitle());
-        AddressEntity address = new AddressEntity();
-        address.generateId();
-        address.setCountry(property.getAddress().getCountry());
-        address.setProvince(property.getAddress().getProvince());
-        address.setCity(property.getAddress().getCity());
-        address.setStreet(property.getAddress().getStreet());
-        address.setZip(property.getAddress().getZip());
-        buildingEntity.setAddress(address);
-        buildingRepository.persistAndFlush(buildingEntity);
-        return buildingEntity;
+        try {
+            BuildingEntity buildingEntity = new BuildingEntity();
+            buildingEntity.setProjectId(projectId);
+            buildingEntity.generateId();
+            buildingEntity.setPropertyId(building);
+            buildingEntity.setTitle(property.getTitle());
+            AddressEntity address = new AddressEntity();
+            address.generateId();
+            address.setCountry(property.getAddress().getCountry());
+            address.setProvince(property.getAddress().getProvince());
+            address.setCity(property.getAddress().getCity());
+            address.setStreet(property.getAddress().getStreet());
+            address.setZip(property.getAddress().getZip());
+            buildingEntity.setAddress(address);
+            buildingRepository.persistAndFlush(buildingEntity);
+            return buildingEntity;
+        } catch (NoResultException e) {
+            throw new WebApplicationException("Building not created", Response.Status.BAD_REQUEST);
+        }
     }
 import de.remsfal.core.model.project.ApartmentModel;
 import de.remsfal.core.model.project.BuildingModel;
--- remsfal-service/src/test/java/de/remsfal/service/boundary/BuildingResourceTest.java
+++ remsfal-service/src/test/java/de/remsfal/service/boundary/BuildingResourceTest.java
@@ -5,7 +5,6 @@ import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -14,13 +13,12 @@ import java.time.Duration;
 import static io.restassured.RestAssured.given;
 
 @QuarkusTest
-public class BuildingResourceTest extends AbstractResourceTest{
+public class BuildingResourceTest extends AbstractResourceTest {
 
     static final String BASE_PATH_GET = "/api/v1/projects/b9440c43-b5c0-4951-9c28-000000000001/properties/b9440c43-b5c0-4951-9c25-000000000001/buildings/b9440c43-b5c0-4951-9c25-000000000001";
 
-    static final String BASE_PATH_POST = "/api/v1/projects/b9440c43-b5c0-4951-9c28-000000000001/properties/b9440c43-b5c0-4951-9c25-000000000001/buildings/";
-
     static final String BASE_PATH = "/api/v1/projects";
+
     @BeforeEach
     protected void setupTestUsers() {
         super.setupTestUsers();
@@ -61,7 +59,7 @@ public class BuildingResourceTest extends AbstractResourceTest{
 
 
         final String json1 = "{ \"title\":\"" + TestData.PROPERTY_TITLE + "\"}";
-        final String user1property1 =  given()
+        final String user1property1 = given()
                 .when()
                 .cookie(buildCookie(TestData.USER_ID, TestData.USER_EMAIL, Duration.ofMinutes(10)))
                 .contentType(ContentType.JSON)
@@ -69,7 +67,8 @@ public class BuildingResourceTest extends AbstractResourceTest{
                 .post(BASE_PATH + "/" + user1project1 + "/properties")
                 .then()
                 .statusCode(Response.Status.CREATED.getStatusCode())
-                .extract().path("id");;
+                .extract().path("id");
+        ;
 
 
         final String user1building1 = given()
@@ -80,7 +79,7 @@ public class BuildingResourceTest extends AbstractResourceTest{
                         " \"livingSpace\":\"" + TestData.APARTMENT_LIVING_SPACE_1 + "\"," +
                         " \"title\":\"" + TestData.BUILDING_TITLE_1 + "\"," +
                         " \"commercialSpace\":\"" + TestData.COMMERCIAL_COMMERCIAL_SPACE_1 + "\"," +
-                        " \"usableSpace\":\"" + TestData.BUILDING_USABLE_SPACE_1+ "\"," +
+                        " \"usableSpace\":\"" + TestData.BUILDING_USABLE_SPACE_1 + "\"," +
                         " \"heatingSpace\":\"" + TestData.APARTMENT_HEATING_SPACE_1 + "\"," +
                         " \"rent\":\"" + TestData.BUILDING_RENT_1 + "\"," +
                         " \"address\": {" +
@@ -96,13 +95,102 @@ public class BuildingResourceTest extends AbstractResourceTest{
                 .extract().path("id");
 
 
-     /*   given()
+        given()
                 .when()
                 .cookie(buildCookie(TestData.USER_ID, TestData.USER_EMAIL, Duration.ofMinutes(10)))
                 .contentType(MediaType.APPLICATION_JSON)
-                .get(BASE_PATH_GET)
+                .get("/api/v1/projects/" + user1project1 + "/properties/" + user1property1 + "/buildings/" + user1building1)
                 .then()
-                .statusCode(Response.Status.OK.getStatusCode());*/
+                .statusCode(Response.Status.OK.getStatusCode());
     }
 
+    @Test
+    void createBuilding_SUCCESS() {
+        final String json = "{ \"title\":\"" + TestData.PROJECT_TITLE + "\"}";
+        final String user1project1 = given()
+                .when()
+                .cookie(buildCookie(TestData.USER_ID, TestData.USER_EMAIL, Duration.ofMinutes(10)))
+                .contentType(ContentType.JSON)
+                .body(json)
+                .post(BASE_PATH)
+                .then()
+                .statusCode(Response.Status.CREATED.getStatusCode())
+                .extract().path("id");
+        final String json1 = "{ \"title\":\"" + TestData.PROPERTY_TITLE + "\"}";
+        final String user1property1 = given()
+                .when()
+                .cookie(buildCookie(TestData.USER_ID, TestData.USER_EMAIL, Duration.ofMinutes(10)))
+                .contentType(ContentType.JSON)
+                .body(json1)
+                .post(BASE_PATH + "/" + user1project1 + "/properties")
+                .then()
+                .statusCode(Response.Status.CREATED.getStatusCode())
+                .extract().path("id");
+        given()
+                .when()
+                .cookie(buildCookie(TestData.USER_ID_1, TestData.USER_EMAIL_1, Duration.ofMinutes(10)))
+                .contentType(MediaType.APPLICATION_JSON)
+                .body("{ \"description\":\"" + TestData.BUILDING_DESCRIPTION_1 + "\"," +
+                        " \"livingSpace\":\"" + TestData.APARTMENT_LIVING_SPACE_1 + "\"," +
+                        " \"title\":\"" + TestData.BUILDING_TITLE_1 + "\"," +
+                        " \"commercialSpace\":\"" + TestData.COMMERCIAL_COMMERCIAL_SPACE_1 + "\"," +
+                        " \"usableSpace\":\"" + TestData.BUILDING_USABLE_SPACE_1 + "\"," +
+                        " \"heatingSpace\":\"" + TestData.APARTMENT_HEATING_SPACE_1 + "\"," +
+                        " \"rent\":\"" + TestData.BUILDING_RENT_1 + "\"," +
+                        " \"address\": {" +
+                        "     \"street\": \"" + TestData.ADDRESS_STREET_1 + "\"," +
+                        "     \"city\": \"" + TestData.ADDRESS_CITY_1 + "\"," +
+                        "     \"province\": \"" + TestData.ADDRESS_PROVINCE_1 + "\"," +
+                        "     \"zip\": \"" + TestData.ADDRESS_ZIP_1 + "\"," +
+                        "     \"country\": \"" + TestData.ADDRESS_COUNTRY_1 + "\"" +
+                        " } }")
+                .post("/api/v1/projects/" + user1project1 + "/properties/" + user1property1 + "/buildings")
+                .then()
+                .statusCode(Response.Status.CREATED.getStatusCode());
+    }
+
+    @Test
+    void createBuilding_FAILED() {
+        final String json = "{ \"title\":\"" + TestData.PROJECT_TITLE + "\"}";
+        final String user1project1 = given()
+                .when()
+                .cookie(buildCookie(TestData.USER_ID, TestData.USER_EMAIL, Duration.ofMinutes(10)))
+                .contentType(ContentType.JSON)
+                .body(json)
+                .post(BASE_PATH)
+                .then()
+                .statusCode(Response.Status.CREATED.getStatusCode())
+                .extract().path("id");
+        final String json1 = "{ \"title\":\"" + TestData.PROPERTY_TITLE + "\"}";
+        final String user1property1 = given()
+                .when()
+                .cookie(buildCookie(TestData.USER_ID, TestData.USER_EMAIL, Duration.ofMinutes(10)))
+                .contentType(ContentType.JSON)
+                .body(json1)
+                .post(BASE_PATH + "/" + user1project1 + "/properties")
+                .then()
+                .statusCode(Response.Status.CREATED.getStatusCode())
+                .extract().path("id");
+        given()
+                .when()
+                .cookie(buildCookie(TestData.USER_ID_1, TestData.USER_EMAIL_1, Duration.ofMinutes(10)))
+                .contentType(MediaType.APPLICATION_JSON)
+                .body("{ \"description\":\"" + TestData.BUILDING_DESCRIPTION_1 + "\"," +
+                        " \"livingSpace\":\"" + TestData.APARTMENT_LIVING_SPACE_1 + "\"," +
+                        " \"title\":\"" + TestData.BUILDING_TITLE_1 + "\"," +
+                        " \"commercialSpace\":\"" + TestData.COMMERCIAL_COMMERCIAL_SPACE_1 + "\"," +
+                        " \"usableSpace\":\"" + TestData.BUILDING_USABLE_SPACE_1 + "\"," +
+                        " \"heatingSpace\":\"" + TestData.APARTMENT_HEATING_SPACE_1 + "\"," +
+                        " \"rent\":\"" + TestData.BUILDING_RENT_1 + "\"," +
+                        " \"address\": {" +
+                        "     \"street\": \"" + TestData.ADDRESS_STREET_1 + "\"," +
+                        "     \"city\": \"" + TestData.ADDRESS_CITY_1 + "\"," +
+                        "     \"province\": \"" + TestData.ADDRESS_PROVINCE_1 + "\"," +
+                        "     \"zip\": \"" + TestData.ADDRESS_ZIP_1 + "\"," +
+                        "     \"country\": \"" + TestData.ADDRESS_COUNTRY_1 + "\"" +
+                        " } }")
+                .post("/api/v1/projects/" + user1project1 + "/properties/" + user1project1 + "/buildings")
+                .then()
+                .statusCode(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode());
+    }
 }
