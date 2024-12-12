af4b1b32c54feae43be23e4489c2c17a019c0c10
Lennard Dubhorn <Lennard.Dubhorn@Student.HTW-Berlin.de>
2024-12-12 01:06:04 +0100

    added Assertions to ApartmentJsonTest & refactored

--- remsfal-core/src/test/java/de/remsfal/core/json/ApartmentJsonTest.java
+++ remsfal-core/src/test/java/de/remsfal/core/json/ApartmentJsonTest.java
@@ -12,6 +12,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ApartmentJsonTest {
 
@@ -22,7 +23,7 @@ public class ApartmentJsonTest {
                 "1. OG rechts",
                 "Frisch renoviert, Fliesen im Flur, Parkett im Wohnzimmer",
                 77.36f, 0f, 77.36f,
-                null); //null because method has to be tested in it's assigned test class
+                null); //null because TenancyModel.valueOf() method has to be tested in it's assigned test class
 
         ApartmentJson apartmentJson = ApartmentJson.valueOf(apartmentModel);
         String apartmentJsonString = "ApartmentJson{id=b9440c43-b5c0-4951-9c24-000000000001," +
@@ -31,52 +32,26 @@ public class ApartmentJsonTest {
                 " heatingSpace=77.36, tenancy=null}";
 
         assertEquals(apartmentJsonString, apartmentJson.toString());
+        assertNotNull(apartmentJson);
+        assertEquals(apartmentModel.getId(), apartmentJson.getId());
+        assertEquals(apartmentModel.getTitle(), apartmentJson.getTitle());
+        assertEquals(apartmentModel.getLocation(), apartmentJson.getLocation());
+        assertEquals(apartmentModel.getDescription(), apartmentJson.getDescription());
+        assertEquals(apartmentModel.getLivingSpace(), apartmentJson.getLivingSpace());
+        assertEquals(apartmentModel.getUsableSpace(), apartmentJson.getUsableSpace());
+        assertEquals(apartmentModel.getHeatingSpace(), apartmentJson.getHeatingSpace());
+        assertEquals(apartmentModel.getTenancy(), apartmentJson.getTenancy());
     }
 
-    class TenancyModelEntity implements TenancyModel {
-        private String id;
-        private LocalDate start;
-        private LocalDate end;
-        private List<? extends RentModel> rent;
-        private CustomerModel tenant;
-
-        public TenancyModelEntity(String id, LocalDate start, LocalDate end) {
-            this.id = id;
-            this.start = start;
-            this.end = end;
-        }
-
-        @Override
-        public String getId() {
-            return id;
-        }
-
-        public LocalDate getStartOfRental() {
-            return start;
-        }
-
-        public LocalDate getEndOfRental() {
-            return end;
-        }
-
-        public List<? extends RentModel> getRent() {
-            return rent;
-        }
-
-        public CustomerModel getTenant() {
-            return tenant;
-        }
-    }
-
-    class ApartmentModelEntity implements ApartmentModel {
-        private String id;
-        private String title;
-        private String location;
-        private String description;
-        private Float livingSpace;
-        private Float usableSpace;
-        private Float heatingSpace;
-        private TenancyModel tenancy;
+    static class ApartmentModelEntity implements ApartmentModel {
+        final private String id;
+        final private String title;
+        final private String location;
+        final private String description;
+        final private Float livingSpace;
+        final private Float usableSpace;
+        final private Float heatingSpace;
+        final private TenancyModel tenancy;
 
         public ApartmentModelEntity(String id, String title, String location, String description,
                                     Float livingSpace, Float usableSpace, Float heatingSpace, TenancyModel tenancy) {
