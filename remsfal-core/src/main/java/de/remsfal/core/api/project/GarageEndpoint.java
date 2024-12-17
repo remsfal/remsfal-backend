038af3a2d6b7c352eba908ff973c9d3d258f0681
Eyad Wahdan <Eyadwahdan02@gmail.com>
2024-12-17 16:55:49 +0100

    Fix Checkstyle Error

--- remsfal-core/src/main/java/de/remsfal/core/api/project/GarageEndpoint.java
+++ remsfal-core/src/main/java/de/remsfal/core/api/project/GarageEndpoint.java
@@ -5,7 +5,14 @@ import de.remsfal.core.validation.UUID;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.groups.ConvertGroup;
-import jakarta.ws.rs.*;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PATCH;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
