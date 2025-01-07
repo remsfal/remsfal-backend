b6367f95fec859fa74629a10c921b9fab0232720
RemodifyBerlin <remodifyBerlin@gmail.com>
2025-01-07 16:35:14 +0100

    fix in ZeebeController

--- remsfal-service/src/main/java/de/remsfal/service/control/ZeebeController.java
+++ remsfal-service/src/main/java/de/remsfal/service/control/ZeebeController.java
@@ -2,6 +2,7 @@ package de.remsfal.service.control;
 
 import io.camunda.zeebe.client.ZeebeClient;
 import jakarta.inject.Inject;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.Response;
 
@@ -19,6 +20,7 @@ public class ZeebeController {
         @Inject
         Logger logger;
 
+        @POST
         @Path("/start")
         public Response startWorkflow(Map<String, Object> requestBody) {
                 try {
