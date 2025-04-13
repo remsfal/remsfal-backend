10fa724cce67346e938de4561bb16f0d87df782f
Alexander Stanik <astanik@users.noreply.github.com>
2025-04-13 09:05:23 +0200

    first simple microservice (#331)

---
+++ remsfal-notification/src/test/java/de/remsfal/notification/boundary/DummyResourceTest.java
@@ -0,0 +1,24 @@
+package de.remsfal.notification.boundary;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+import jakarta.ws.rs.core.Response.Status;
+
+@QuarkusTest
+class DummyResourceTest {
+
+    static final String BASE_PATH = "/api/v1/address";
+
+    @Test
+    void getSupportedCountries_SUCCESS_nullIsReturned() {
+        given()
+            .when()
+            .get(BASE_PATH + "/countries")
+            .then()
+            .statusCode(Status.NO_CONTENT.getStatusCode());
+    }
+
+}
