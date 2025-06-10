3ab62c32ee9cae8467f12ccf271cdbb51fcf23e6
s0589012 <Alona.Vasylchenko@Student.HTW-Berlin.de>
2025-06-10 23:48:33 +0200

    Fix tests

--- remsfal-services/remsfal-notification/src/test/java/de/remsfal/notification/resource/MailResourceTest.java
+++ remsfal-services/remsfal-notification/src/test/java/de/remsfal/notification/resource/MailResourceTest.java
@@ -1,46 +1,60 @@
 package de.remsfal.notification.resource;
 
 import io.quarkus.test.junit.QuarkusTest;
-import io.restassured.RestAssured;
-import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
 import org.junit.jupiter.api.Test;
+
 import static io.restassured.RestAssured.given;
 
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.containsString;
-
 @QuarkusTest
-public class MailResourceTest {
+class MailResourceTest {
+
+    static final String BASE_PATH = "/notification/test";
 
     @Test
-    public void testWelcomeTemplateResponse() {
-        RestAssured.given()
+    void sendWelcomeTemplate_SUCCESS() {
+        given()
                 .queryParam("to", "test@example.com")
                 .queryParam("subject", "Welcome")
                 .queryParam("name", "TestUser")
                 .queryParam("template", "welcome")
                 .queryParam("token", "abc123")
                 .when()
-                .get("/notification/test")
+                .get(BASE_PATH)
                 .then()
-                .statusCode(Response.Status.ACCEPTED.getStatusCode());
+                .statusCode(Status.ACCEPTED.getStatusCode());
     }
 
     @Test
-    public void testMissingParams() {
-        RestAssured.given()
+    void sendNewMembershipTemplate_SUCCESS() {
+        given()
+                .queryParam("to", "test@example.com")
+                .queryParam("subject", "Welcome")
+                .queryParam("name", "TestUser")
+                .queryParam("template", "new-membership")
+                .queryParam("token", "abc123")
+                .when()
+                .get(BASE_PATH)
+                .then()
+                .statusCode(Status.ACCEPTED.getStatusCode());
+    }
+
+    @Test
+    void missingParams_FAILED() {
+        given()
                 .queryParam("name", "NoRecipient")
                 .when()
-                .get("/notification/test")
+                .get(BASE_PATH)
                 .then()
-                .statusCode(500); // because IllegalArgumentException will be thrown
+                .statusCode(Status.INTERNAL_SERVER_ERROR.getStatusCode());
     }
+
     @Test
-    public void testMissingRequiredParameters() {
+    void missingRequiredParameters_FAILED() {
         given()
                 .when()
-                .get("/notification/test")  // without required query parameters
+                .get(BASE_PATH)
                 .then()
-                .statusCode(500);
+                .statusCode(Status.INTERNAL_SERVER_ERROR.getStatusCode());
     }
 }
