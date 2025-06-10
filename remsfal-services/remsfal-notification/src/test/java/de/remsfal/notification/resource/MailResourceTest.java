d47c99a4bdd6fd63d7628b0194577c7e3cc238c9
s0589012 <Alona.Vasylchenko@Student.HTW-Berlin.de>
2025-06-10 19:39:48 +0200

    Fix test coverage

--- remsfal-services/remsfal-notification/src/test/java/de/remsfal/notification/resource/MailResourceTest.java
+++ remsfal-services/remsfal-notification/src/test/java/de/remsfal/notification/resource/MailResourceTest.java
@@ -4,6 +4,7 @@ import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.RestAssured;
 import jakarta.ws.rs.core.Response;
 import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
 
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.containsString;
@@ -33,5 +34,13 @@ public class MailResourceTest {
                 .get("/notification/test")
                 .then()
                 .statusCode(500); // because IllegalArgumentException will be thrown
+    }
+    @Test
+    public void testMissingRequiredParameters() {
+        given()
+                .when()
+                .get("/notification/test")  // without required query parameters
+                .then()
+                .statusCode(500);
     }
 }
