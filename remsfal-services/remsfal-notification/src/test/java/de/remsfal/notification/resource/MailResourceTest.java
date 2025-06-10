231a6bb32148c5e66b5f84ae566bf4a14167dcf0
s0589012 <Alona.Vasylchenko@Student.HTW-Berlin.de>
2025-06-10 17:47:52 +0200

    Add MailResourceTest with basic endpoint tests

---
+++ remsfal-services/remsfal-notification/src/test/java/de/remsfal/notification/resource/MailResourceTest.java
@@ -0,0 +1,37 @@
+package de.remsfal.notification.resource;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.RestAssured;
+import jakarta.ws.rs.core.Response;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.containsString;
+
+@QuarkusTest
+public class MailResourceTest {
+
+    @Test
+    public void testWelcomeTemplateResponse() {
+        RestAssured.given()
+                .queryParam("to", "test@example.com")
+                .queryParam("subject", "Welcome")
+                .queryParam("name", "TestUser")
+                .queryParam("template", "welcome")
+                .queryParam("token", "abc123")
+                .when()
+                .get("/notification/test")
+                .then()
+                .statusCode(Response.Status.ACCEPTED.getStatusCode());
+    }
+
+    @Test
+    public void testMissingParams() {
+        RestAssured.given()
+                .queryParam("name", "NoRecipient")
+                .when()
+                .get("/notification/test")
+                .then()
+                .statusCode(500); // because IllegalArgumentException will be thrown
+    }
+}
