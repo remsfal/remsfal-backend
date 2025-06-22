110bc739ec9529cca6a8e89e6b517f9dfcf8b9a2
AlexBürklen <Alexander.Buerklen@Student.HTW-Berlin.de>
2025-06-22 17:07:01 +0200

    - added quarkus integration of kafka in application.properties - adapted docker-compose.yml for configuration of kafka-ui and backend - initial implementation for producing events in NotificationController.java - changed quarkus-kafka dependency to the correct one

--- remsfal-services/remsfal-platform/src/main/java/de/remsfal/service/control/NotificationController.java
+++ remsfal-services/remsfal-platform/src/main/java/de/remsfal/service/control/NotificationController.java
@@ -1,23 +1,27 @@
 package de.remsfal.service.control;
 
+import de.remsfal.core.model.CustomerModel;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
 import org.jboss.logging.Logger;
-
 import de.remsfal.core.model.UserModel;
+import de.remsfal.core.json.UserJson;
 
-/**
- * @author Alexander Stanik [alexander.stanik@htw-berlin.de]
- */
 @ApplicationScoped
 public class NotificationController {
-    
+
     @Inject
     Logger logger;
-    
-    public void informUserAboutProjectMembership(final UserModel user) {
-        logger.infov("TODO: User {0} will be informed about project membership", user.getEmail());
-    }
 
+    @Inject
+    @Channel("user-notification")
+    Emitter<UserJson> notificationEmitter;
+
+    public void informUserAboutProjectMembership(final UserModel user) {
+        UserJson json = UserJson.valueOf((CustomerModel) user);
+        logger.infov("Sending user-notification for {0}", json.getEmail());
+        notificationEmitter.send(json);
+    }
 }
