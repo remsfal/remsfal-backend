519132415469ee2e7ab323b6c3ab418aac252d96
AlexBürklen <Alexander.Buerklen@Student.HTW-Berlin.de>
2025-06-26 14:26:16 +0200

    - modified test

--- remsfal-services/remsfal-platform/src/test/java/de/remsfal/service/control/NotificationControllerTest.java
+++ remsfal-services/remsfal-platform/src/test/java/de/remsfal/service/control/NotificationControllerTest.java
@@ -11,9 +11,13 @@ import io.quarkus.test.kafka.KafkaCompanionResource;
 import io.smallrye.reactive.messaging.kafka.companion.ConsumerTask;
 import io.smallrye.reactive.messaging.kafka.companion.KafkaCompanion;
 import jakarta.inject.Inject;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import io.quarkus.kafka.client.serialization.ObjectMapperDeserializer;
+import io.quarkus.kafka.client.serialization.ObjectMapperSerializer;
+import org.apache.kafka.common.serialization.Serdes;
+
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 import static de.remsfal.service.TestData.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -28,23 +32,29 @@ class NotificationControllerTest extends AbstractTest {
     @InjectKafkaCompanion
     KafkaCompanion companion;
 
+    @BeforeEach
+    void setup() {
+        companion.registerSerde(UserJson.class, Serdes.serdeFrom(
+                new ObjectMapperSerializer<>(),
+                new ObjectMapperDeserializer<>(UserJson.class)
+        ));
+    }
+
     @Test
     void testNotificationIsSentToKafkaWithAllFieldsExceptAddress() {
         String topic = "user-notification";
 
-
         UserJson user = ImmutableUserJson.builder()
-                .id(TestData.USER_ID)
-                .email(TestData.USER_EMAIL)
-                .firstName(TestData.USER_FIRST_NAME)
-                .lastName(TestData.USER_LAST_NAME)
+                .id(TestData.USER_ID_2)
+                .email(TestData.USER_EMAIL_2)
+                .firstName(TestData.USER_FIRST_NAME_2)
+                .lastName(TestData.USER_LAST_NAME_2)
                 .address(addressBuilder1().build())
-                .mobilePhoneNumber("+49123456789")
-                .businessPhoneNumber("+4987654321")
-                .privatePhoneNumber("+4901234567")
-                .registeredDate(LocalDate.of(2020, 5, 20))
-                .lastLoginDate(LocalDateTime.of(2025, 6, 24, 15, 30))
-                .active(Boolean.TRUE)
+                .mobilePhoneNumber(null)
+                .businessPhoneNumber(null)
+                .privatePhoneNumber(null)
+                .registeredDate(null)
+                .lastLoginDate(null)
                 .build();
 
         notificationController.informUserAboutProjectMembership(user);
@@ -53,21 +63,22 @@ class NotificationControllerTest extends AbstractTest {
                 .fromTopics(topic, 1);
 
         task.awaitCompletion();
+        logger.infov("task: " + task);
 
         assertEquals(1, task.count());
         UserJson payload = task.getRecords().get(0).value();
+        logger.infov("payload: " + payload.toString());
 
-        assertEquals(TestData.USER_ID, payload.getId());
-        assertEquals(TestData.USER_EMAIL, payload.getEmail());
+        assertEquals(TestData.USER_ID_2, payload.getId());
+        assertEquals(TestData.USER_EMAIL_2, payload.getEmail());
         assertNull(payload.getName());
-        assertEquals(TestData.USER_FIRST_NAME, payload.getFirstName());
-        assertEquals(TestData.USER_LAST_NAME, payload.getLastName());
-        assertEquals(addressBuilder1().build(), payload.getAddress());
-        assertEquals("+49123456789", payload.getMobilePhoneNumber());
-        assertEquals("+4987654321", payload.getBusinessPhoneNumber());
-        assertEquals("+4901234567", payload.getPrivatePhoneNumber());
-        assertEquals(LocalDate.of(2020, 5, 20), payload.getRegisteredDate());
-        assertEquals(LocalDateTime.of(2025, 6, 24, 15, 30), payload.getLastLoginDate());
-        assertTrue(payload.isActive());
+        assertEquals(TestData.USER_FIRST_NAME_2, payload.getFirstName());
+        assertEquals(TestData.USER_LAST_NAME_2, payload.getLastName());
+        assertEquals(null, payload.getAddress());
+        assertNull(null, payload.getMobilePhoneNumber());
+        assertNull(null, payload.getBusinessPhoneNumber());
+        assertNull(null, payload.getPrivatePhoneNumber());
+        assertEquals(LocalDate.of(2025, 6, 26), payload.getRegisteredDate());
+        assertEquals(null, payload.getLastLoginDate());
     }
 }
