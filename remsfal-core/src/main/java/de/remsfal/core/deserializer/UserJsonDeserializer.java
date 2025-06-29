f3e048b4599c96b631089e015e2f0bd5e81be79f
AlexBürklen <Alexander.Buerklen@Student.HTW-Berlin.de>
2025-06-29 16:22:56 +0200

    - adapted consumer and producer in kafka configuration - adapted docker-compose.yml for correct access to kafka-ui and connection for quarkus application - initial implementation for kafka consumer - added UserJsonDeserializer - added new dependency "quarkus-kafka-client" for UserJsonDeserializer

---
+++ remsfal-core/src/main/java/de/remsfal/core/deserializer/UserJsonDeserializer.java
@@ -0,0 +1,10 @@
+package de.remsfal.core.deserializer;
+
+import de.remsfal.core.json.UserJson;
+import io.quarkus.kafka.client.serialization.ObjectMapperDeserializer;
+
+public class UserJsonDeserializer extends ObjectMapperDeserializer<UserJson> {
+    public UserJsonDeserializer() {
+        super(UserJson.class);
+    }
+}
--- remsfal-services/remsfal-notification/src/main/java/de/remsfal/notification/boundary/DummyResource.java
+++ remsfal-services/remsfal-notification/src/main/java/de/remsfal/notification/boundary/DummyResource.java
@@ -3,13 +3,29 @@ package de.remsfal.notification.boundary;
 import de.remsfal.core.api.AddressEndpoint;
 import de.remsfal.core.json.AddressJson;
 import de.remsfal.core.json.CountryListJson;
+import de.remsfal.core.json.UserJson;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.jboss.logging.Logger;
 
 import java.util.List;
 
 /**
  * @author Alexander Stanik [alexander.stanik@htw-berlin.de]
  */
+
+@ApplicationScoped
 public class DummyResource implements AddressEndpoint {
+
+    @Inject
+    Logger logger;
+
+    @Incoming("user-notification-consumer")
+    public void consumeUserNotification(UserJson userJson) {
+        logger.infov("Received user-notification for {0}", userJson.getEmail());
+        logger.debugf("Full user data: %s", userJson);
+    }
 
     @Override
     public CountryListJson getSupportedCountries() {
@@ -20,5 +36,4 @@ public class DummyResource implements AddressEndpoint {
     public List<AddressJson> getPossibleCities(final String zipCode) {
         return null;
     }
-
 }
