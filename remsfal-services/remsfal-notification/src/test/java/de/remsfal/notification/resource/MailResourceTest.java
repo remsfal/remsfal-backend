9634661bca5b737586db83d6bcae82318c9b901e
Alexander Stanik <alexander.stanik@htw-berlin.de>
2025-07-02 17:12:46 +0200

    added mailing tests

--- remsfal-services/remsfal-notification/src/test/java/de/remsfal/notification/resource/MailResourceTest.java
+++ remsfal-services/remsfal-notification/src/test/java/de/remsfal/notification/resource/MailResourceTest.java
@@ -1,15 +1,33 @@
 package de.remsfal.notification.resource;
 
+import io.quarkus.mailer.MockMailbox;
 import io.quarkus.test.junit.QuarkusTest;
+import io.vertx.ext.mail.MailMessage;
 import jakarta.ws.rs.core.Response.Status;
 import org.junit.jupiter.api.Test;
+import jakarta.inject.Inject;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import java.io.IOException;
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @QuarkusTest
 class MailResourceTest {
 
     static final String BASE_PATH = "/notification/test";
+
+    @Inject
+    MockMailbox mailbox;
+
+    @BeforeEach
+    void init() {
+        mailbox.clear();
+    }
 
     @Test
     void shouldFail_whenMissingRecipient() {
@@ -32,5 +50,27 @@ class MailResourceTest {
                 .get(BASE_PATH)
                 .then()
                 .statusCode(422);
+    }
+
+    @Test
+    void testTextMail() throws IOException {
+        // call a REST endpoint that sends email
+        given()
+                .queryParam("to", "test@example.com")
+                .queryParam("name", "TestUser")
+                .queryParam("template", "new-membership")
+                .queryParam("link", "https://remsfal.de")
+                .when()
+                .get(BASE_PATH)
+                .then()
+                .statusCode(Status.ACCEPTED.getStatusCode());
+
+        // verify that it was sent
+        List<MailMessage> sent = mailbox. getMailMessagesSentTo("test@example.com");
+        assertEquals(1, sent.size());
+        MailMessage actual = sent.get(0);
+        assertTrue(actual.getHtml().contains("You have been added to a new project."));
+        assertEquals("You’ve been added to a new project", actual.getSubject());
+        assertEquals(1, mailbox.getTotalMessagesSent());
     }
 }
