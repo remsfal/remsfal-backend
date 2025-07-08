34a23ae67aa0bcc744d1b923215e76f64285eb0a
Christopher <christopher.keller@student.htw-berlin.de>
2025-07-08 21:45:24 +0200

    merge kafka configurations

--- remsfal-services/remsfal-chat/src/test/java/de/remsfal/chat/resource/OcrServiceResource.java
+++ remsfal-services/remsfal-chat/src/test/java/de/remsfal/chat/resource/OcrServiceResource.java
@@ -27,25 +27,25 @@ public class OcrServiceResource implements QuarkusTestResourceLifecycleManager, 
 
         kafkaContainer = new GenericContainer<>("apache/kafka:latest")
                 .withNetwork(network)
-                .withNetworkAliases("kafka")
+                .withNetworkAliases("kafka-broker")
                 .withEnv("KAFKA_NODE_ID", "1")
                 .withEnv("KAFKA_PROCESS_ROLES", "broker,controller")
-                .withEnv("KAFKA_CONTROLLER_QUORUM_VOTERS", "1@kafka:9093")
+                .withEnv("KAFKA_CONTROLLER_QUORUM_VOTERS", "1@kafka-broker:9093")
                 .withEnv("KAFKA_CONTROLLER_LISTENER_NAMES", "CONTROLLER")
-                .withEnv("KAFKA_LISTENERS", "DOCKER_INTERNAL://kafka:29092,HOST_EXTERNAL://0.0.0.0:9092,CONTROLLER://kafka:9093")
-                .withEnv("KAFKA_ADVERTISED_LISTENERS", "DOCKER_INTERNAL://kafka:29092,HOST_EXTERNAL://localhost:9092")
-                .withEnv("KAFKA_LISTENER_SECURITY_PROTOCOL_MAP", "DOCKER_INTERNAL:PLAINTEXT,HOST_EXTERNAL:PLAINTEXT,CONTROLLER:PLAINTEXT")
-                .withEnv("KAFKA_INTER_BROKER_LISTENER_NAME", "DOCKER_INTERNAL")
+                .withEnv("KAFKA_LISTENERS", "INTERNAL://0.0.0.0:29092,EXTERNAL://0.0.0.0:9092,HOST://0.0.0.0:39092,CONTROLLER://0.0.0.0:9093")
+                .withEnv("KAFKA_ADVERTISED_LISTENERS", "INTERNAL://kafka-broker:29092,EXTERNAL://kafka-broker:9092,HOST://localhost:39092")
+                .withEnv("KAFKA_LISTENER_SECURITY_PROTOCOL_MAP", "INTERNAL:PLAINTEXT,EXTERNAL:PLAINTEXT,HOST:PLAINTEXT,CONTROLLER:PLAINTEXT")
+                .withEnv("KAFKA_INTER_BROKER_LISTENER_NAME", "INTERNAL")
                 .withEnv("KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR", "1")
                 .withEnv("KAFKA_TRANSACTION_STATE_LOG_REPLICATION_FACTOR", "1")
                 .withEnv("KAFKA_TRANSACTION_STATE_LOG_MIN_ISR", "1")
                 .withEnv("KAFKA_GROUP_INITIAL_REBALANCE_DELAY_MS", "0")
                 .withEnv("KAFKA_NUM_PARTITIONS", "1")
-                .withExposedPorts(9092);
+                .withExposedPorts(39092, 9092);
 
         ocrContainer = new GenericContainer<>("ghcr.io/remsfal/remsfal-ocr:sha-a1f0971")
                 .withNetwork(network)
-                .withEnv("KAFKA_BROKER", "kafka:29092")
+                .withEnv("KAFKA_BROKER", "kafka-broker:29092")
                 .withEnv("MINIO_ENDPOINT", "minio:9000")
                 .withEnv("PYTHONUNBUFFERED", "1")
                 .waitingFor(Wait.forLogMessage(".*Listening to topic .*\\n", 1))
@@ -59,7 +59,7 @@ public class OcrServiceResource implements QuarkusTestResourceLifecycleManager, 
                 .withEnv("MINIO_ROOT_PASSWORD", "minioadminpassword")
                 .withExposedPorts(9000);
 
-        kafkaContainer.setPortBindings(List.of("9092:9092"));
+        kafkaContainer.setPortBindings(List.of("39092:39092"));
         minioContainer.setPortBindings(List.of("9000:9000"));
 
         kafkaContainer.start();
