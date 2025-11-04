53ef5ad55705857c2592d285933b5c129d19224c
amelie0298 <seidel1998@gmx.de>
2025-11-04 21:17:13 +0100

    Fix ECBArchitectureTest rules to correctly enforce Entity-Control-Boundary dependencies
    Adjusted ArchUnit test rules and allowed package dependencies (e.g., io.smallrye.jwt, com.google.api.client, de.remsfal.common.authentication).
    This fixes false positives and ensures correct ECB layer separation.
    
    Closes #32
    
    Signed-off-by: amelie0298 <seidel1998@gmx.de>

---
+++ remsfal-services/remsfal-platform/src/test/java/de/remsfal/service/ECBArchitectureTest.java
@@ -0,0 +1,95 @@
+package de.remsfal.service;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.lang.ArchRule;
+
+import org.junit.jupiter.api.Test;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+
+import static com.tngtech.archunit.core.importer.ImportOption.Predefined.DO_NOT_INCLUDE_TESTS;
+
+public class ECBArchitectureTest {
+
+    private static final String BASE_PACKAGE = "de.remsfal.service";
+
+    @Test
+    void boundary_should_only_access_control() {
+        JavaClasses importedClasses = new ClassFileImporter()
+                .withImportOption(DO_NOT_INCLUDE_TESTS)
+                .importPackages(BASE_PACKAGE);
+
+        ArchRule rule = classes()
+                .that().resideInAPackage("..boundary..")
+                .should().onlyAccessClassesThat()
+                .resideInAnyPackage(
+                        "..boundary..",
+                        "..control..",
+                        "..entity..",
+                        "..dto..",
+                        "..model..",
+                        "de.remsfal.core..",
+                        "de.remsfal.common.authentication..",
+                        "java..",                // Standard Java-Pakete
+                        "jakarta..",             // Jakarta EE
+                        "org.jboss.logging..",   // Logging Framework
+                        "io.quarkus..",          // Quarkus Framework
+                        "org.eclipse.microprofile..",
+                        "com.google.api.client..",
+                        "com.nimbusds.jose..",
+                        "io.smallrye.jwt.auth.principal.."
+                );
+
+        rule.check(importedClasses);
+    }
+
+    @Test
+    void control_should_only_access_entity() {
+        JavaClasses importedClasses = new ClassFileImporter()
+                .withImportOption(DO_NOT_INCLUDE_TESTS)
+                .importPackages(BASE_PACKAGE);
+
+        ArchRule rule = classes()
+                .that().resideInAPackage("..control..")
+                .should().onlyAccessClassesThat()
+                .resideInAnyPackage(
+                        "..entity..", // darf Entities nutzen
+                        "..control..",
+                        "..control.event..",    // falls Events dazugehören
+                        "..dto..",
+                        "..model..",
+                        "de.remsfal.core..",
+                        "de.remsfal.service.entity.dto..",
+                        "de.remsfal.service.boundary.exception..",
+                        "de.remsfal.common.authentication..",
+                        "java..",
+                        "jakarta..",
+                        "org.jboss.logging..",
+                        "io.quarkus..",
+                        "org.eclipse.microprofile..",
+                        "io.smallrye.jwt.auth.principal.."
+                );
+
+
+        rule.check(importedClasses);
+    }
+
+    @Test
+    void entity_should_not_access_boundary_or_control() {
+        JavaClasses importedClasses = new ClassFileImporter()
+                .withImportOption(DO_NOT_INCLUDE_TESTS)
+                .importPackages(BASE_PACKAGE);
+
+        ArchRule rule = noClasses()
+                .that().resideInAPackage("..entity..")
+                .should().accessClassesThat()
+                .resideInAnyPackage(
+                        "..control..",
+                        "..boundary.."
+                );
+
+        rule.check(importedClasses);
+    }
+}
