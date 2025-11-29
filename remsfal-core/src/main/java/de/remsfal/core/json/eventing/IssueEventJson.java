ea02e5297b6e6463ce42c250c4d17397272bd0ac
Christian Koch <c.koch@exval.de>
2025-11-29 17:46:58 +0100

    issue-#564 Define Json Schema for Event in Topic

---
+++ remsfal-core/src/main/java/de/remsfal/core/json/eventing/IssueEventJson.java
@@ -0,0 +1,60 @@
+package de.remsfal.core.json.eventing;
+
+import java.util.UUID;
+
+import org.immutables.value.Value.Immutable;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import de.remsfal.core.ImmutableStyle;
+import de.remsfal.core.json.UserJson;
+import de.remsfal.core.model.ticketing.IssueModel;
+import jakarta.annotation.Nullable;
+
+@Immutable
+@ImmutableStyle
+@JsonDeserialize(as = ImmutableIssueEventJson.class)
+@JsonNaming(PropertyNamingStrategies.LowerCamelCaseStrategy.class)
+public interface IssueEventJson {
+
+    String TOPIC = "issue-events";
+
+    enum IssueEventType {
+        ISSUE_CREATED,
+        ISSUE_UPDATED,
+        ISSUE_ASSIGNED,
+        ISSUE_MENTIONED
+    }
+
+    IssueEventType getType();
+
+    UUID getIssueId();
+
+    UUID getProjectId();
+
+    @Nullable
+    String getTitle();
+
+    @Nullable
+    IssueModel.Type getIssueType();
+
+    @Nullable
+    IssueModel.Status getStatus();
+
+    @Nullable
+    UserJson getUser();
+
+    /**
+     * Target user for owner assignment events.
+     */
+    @Nullable
+    UserJson getOwner();
+
+    /**
+     * Target user for mention events.
+     */
+    @Nullable
+    UserJson getMentionedUser();
+}
