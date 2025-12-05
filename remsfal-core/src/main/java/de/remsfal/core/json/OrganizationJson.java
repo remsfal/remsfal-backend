7d9deeee1292f77fb70efb14d463046dae4033c2
Miroslaw-Keil <Miroslaw.Keil@Student.HTW-Berlin.de>
2025-12-05 22:57:10 +0100

    ORM structure for organization management (without employees) almost done

--- remsfal-core/src/main/java/de/remsfal/core/json/OrganizationJson.java
+++ remsfal-core/src/main/java/de/remsfal/core/json/OrganizationJson.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import de.remsfal.core.ImmutableStyle;
-import de.remsfal.core.model.ContractorModel;
 import de.remsfal.core.model.OrganizationModel;
 import de.remsfal.core.validation.PatchValidation;
 import de.remsfal.core.validation.PostValidation;
@@ -12,16 +11,18 @@ import jakarta.annotation.Nullable;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.*;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
-import org.immutables.value.Value.Immutable;
+import org.immutables.value.Value;
 import java.util.UUID;
 
-@Immutable
+/**
+ * JSON representation of an organization
+ */
+@Value.Immutable
 @ImmutableStyle
 @Schema(description = "An organization")
-//TODO: ImmutableOrganizationJson wird nicht erkannt
-//@JsonDeserialize(as = ImmutableOrganizationJson.class)
+@JsonDeserialize(as = ImmutableOrganizationJson.class)
 @JsonNaming(PropertyNamingStrategies.LowerCamelCaseStrategy.class)
-public abstract class OrganizsationJson implements OrganizationModel {
+public abstract class OrganizationJson implements OrganizationModel {
 
     @Null(groups = PostValidation.class)
     @NotNull(groups = PatchValidation.class)
@@ -58,23 +59,22 @@ public abstract class OrganizsationJson implements OrganizationModel {
      * @param model the model
      * @return the JSON representation
      */
-    //TODO: ImmutableOrganizationJson wird nicht erkannt
-//    public static OrganizsationJson valueOf(final OrganizationModel model) {
-//        if (model == null) {
-//            return null;
-//        }
-//
-//        final ImmutableOrganizationJson.Builder builder = ImmutableOrganizationJson.builder()
-//                .id(model.getId())
-//                .name(model.getName())
-//                .phone(model.getPhone())
-//                .email(model.getEmail())
-//                .trade(model.getTrade());
-//
-//        if (model.getAddress() != null) {
-//            builder.address(AddressJson.valueOf(model.getAddress()));
-//        }
-//
-//        return builder.build();
-//    }
+    public static OrganizationJson valueOf(final OrganizationModel model) {
+        if (model == null) {
+            return null;
+        }
+
+        final ImmutableOrganizationJson.Builder builder = ImmutableOrganizationJson.builder()
+                .id(model.getId())
+                .name(model.getName())
+                .phone(model.getPhone())
+                .email(model.getEmail())
+                .trade(model.getTrade());
+
+        if (model.getAddress() != null) {
+            builder.address(AddressJson.valueOf(model.getAddress()));
+        }
+
+        return builder.build();
+    }
 }
