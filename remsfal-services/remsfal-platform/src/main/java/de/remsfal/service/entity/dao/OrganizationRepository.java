c209426b046792c39f8f4c072b315b9ad8ec0742
Miroslaw-Keil <Miroslaw.Keil@Student.HTW-Berlin.de>
2025-12-06 12:17:49 +0100

    Initialized ORM structure for Organization - Employee mapping

--- remsfal-services/remsfal-platform/src/main/java/de/remsfal/service/entity/dao/OrganizationRepository.java
+++ remsfal-services/remsfal-platform/src/main/java/de/remsfal/service/entity/dao/OrganizationRepository.java
@@ -1,6 +1,8 @@
 package de.remsfal.service.entity.dao;
 
+import de.remsfal.service.entity.dto.OrganizationEmployeeEntity;
 import de.remsfal.service.entity.dto.OrganizationEntity;
+import de.remsfal.service.entity.dto.ProjectMembershipEntity;
 import jakarta.enterprise.context.ApplicationScoped;
 
 import java.util.List;
@@ -8,5 +10,10 @@ import java.util.Optional;
 
 @ApplicationScoped
 public class OrganizationRepository extends AbstractRepository<OrganizationEntity> {
+
+    //TODO: Repository implementieren
+    public List<OrganizationEmployeeEntity> findOrganizationEmployeesByOrganizationId(Long organizationId) {
+        return null;
+    }
 
 }
