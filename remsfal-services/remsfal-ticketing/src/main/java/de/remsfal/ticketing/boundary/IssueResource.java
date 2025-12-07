18165612d23e8a285ea8bffafada3723d1b61fac
Max <buningmax@rocketmail.com>
2025-12-07 21:48:53 +0100

    added the deleteRelation function

--- remsfal-services/remsfal-ticketing/src/main/java/de/remsfal/ticketing/boundary/IssueResource.java
+++ remsfal-services/remsfal-ticketing/src/main/java/de/remsfal/ticketing/boundary/IssueResource.java
@@ -145,4 +145,13 @@ public class IssueResource extends AbstractResource implements IssueEndpoint {
         return resourceContext.initResource(chatSessionResource.get());
     }
 
+    @Override
+    public void deleteRelation(UUID issueId, String type, UUID relatedIssueId) {
+        IssueEntity entity = issueController.getIssue(issueId);
+        if (!principal.getProjectRoles().containsKey(entity.getProjectId())) {
+            throw new ForbiddenException("User does not have permission to update this issue");
+        }
+        issueController.deleteRelation(entity, type, relatedIssueId);
+    }
+
 }
