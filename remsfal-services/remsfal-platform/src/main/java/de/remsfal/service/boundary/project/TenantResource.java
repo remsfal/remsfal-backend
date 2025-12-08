6a4d64b821d7c3b577cd2739b8a9f3837396eca0
hienan02 <nghienan02@gmail.com>
2025-12-08 14:07:15 +0100

    Implemented TenantResource

---
+++ remsfal-services/remsfal-platform/src/main/java/de/remsfal/service/boundary/project/TenantResource.java
@@ -0,0 +1,63 @@
+package de.remsfal.service.boundary.project;
+
+import de.remsfal.core.api.project.TenantEndpoint;
+import de.remsfal.core.json.UserJson;
+import de.remsfal.core.model.CustomerModel;
+import de.remsfal.service.control.TenantController;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import java.net.URI;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+
+@RequestScoped
+public class TenantResource extends ProjectSubResource implements TenantEndpoint {
+
+    @Inject
+    TenantController controller;
+
+    @Override
+    public Response createTenant(final UUID projectId, final UserJson tenant) {
+        checkWritePermissions(projectId);
+        final CustomerModel model = controller.createTenant (projectId, tenant);
+        final URI location = uri.getAbsolutePathBuilder().path(model.getId().toString()).build();
+        return Response.created(location)
+                .type(MediaType.APPLICATION_JSON)
+                .entity(UserJson.valueOf(model))
+                .build();
+    }
+
+    @Override
+    public List<UserJson> getTenants(final UUID projectId) {
+        checkReadPermissions(projectId);
+        List<CustomerModel> tenants = controller.getTenants(projectId);
+        return tenants.stream()
+                .map(UserJson::valueOf)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public UserJson getTenant(final UUID projectId, final UUID tenantId) {
+        checkReadPermissions(projectId);
+        final CustomerModel model = controller.getTenant(projectId, tenantId);
+        return UserJson.valueOf(model);
+    }
+
+    @Override
+    public UserJson updateTenant(final UUID projectId, final UUID tenantId, UserJson tenant) {
+        checkWritePermissions(projectId);
+        final CustomerModel model = controller.updateTenant(projectId, tenantId, tenant);
+        return UserJson.valueOf(model);
+    }
+
+    @Override
+    public void deleteTenant(UUID projectId, UUID tenantId) {
+        checkWritePermissions(projectId);
+        controller.deleteTenant(projectId, tenantId);
+    }
+}
