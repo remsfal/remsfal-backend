034d0e06e79ef797c29f7a95159566f7041c4954
carlwilliamrix <carlrix1@gmail.com>
2025-12-08 16:01:17 +0100

    Refactor tenancy management by introducing ProjectTenancyEndpoint seperating the management from the Tenancy Endpoint

--- remsfal-services/remsfal-platform/src/main/java/de/remsfal/service/boundary/ProjectResource.java
+++ remsfal-services/remsfal-platform/src/main/java/de/remsfal/service/boundary/ProjectResource.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.UUID;
 
 import de.remsfal.service.boundary.project.*;
-import de.remsfal.service.boundary.tenancy.TenancyResource;
 import io.quarkus.security.Authenticated;
 import jakarta.enterprise.inject.Instance;
 import jakarta.inject.Inject;
@@ -27,134 +26,135 @@ import de.remsfal.core.model.ProjectModel;
 import de.remsfal.service.control.ProjectController;
 
 import org.eclipse.microprofile.metrics.annotation.Timed;
+
 /**
  * @author Alexander Stanik [alexander.stanik@htw-berlin.de]
  */
 @Authenticated
 public class ProjectResource implements ProjectEndpoint {
 
-    @Context
-    UriInfo uri;
+  @Context
+  UriInfo uri;
 
-    @Context
-    ResourceContext resourceContext;
+  @Context
+  ResourceContext resourceContext;
 
-    @Inject
-    RemsfalPrincipal principal;
+  @Inject
+  RemsfalPrincipal principal;
 
-    @Inject
-    Logger logger;
+  @Inject
+  Logger logger;
 
-    @Inject
-    ProjectController controller;
+  @Inject
+  ProjectController controller;
 
-    @Inject
-    Instance<MemberResource> memberResource;
+  @Inject
+  Instance<MemberResource> memberResource;
 
-    @Inject
-    Instance<PropertyResource> propertyResource;
+  @Inject
+  Instance<PropertyResource> propertyResource;
 
-    @Inject
-    Instance<SiteResource> siteResource;
+  @Inject
+  Instance<SiteResource> siteResource;
 
-    @Inject
-    Instance<BuildingResource> buildingResource;
+  @Inject
+  Instance<BuildingResource> buildingResource;
 
-    @Inject
-    Instance<ApartmentResource> apartmentResource;
+  @Inject
+  Instance<ApartmentResource> apartmentResource;
 
-    @Inject
-    Instance<CommercialResource> commercialResource;
+  @Inject
+  Instance<CommercialResource> commercialResource;
 
-    @Inject
-    Instance<StorageResource> storageResource;
+  @Inject
+  Instance<StorageResource> storageResource;
 
-    @Inject
-    Instance<ContractorResource> contractorResource;
+  @Inject
+  Instance<ContractorResource> contractorResource;
 
-    @Inject
-    Instance<TenancyResource> tenancyResource;
+  @Inject
+  Instance<ProjectTenancyResource> tenancyResource;
 
-    @Override
-    @Timed(name = "GetProjectsListTimer", unit = MetricUnits.MILLISECONDS)
-    public ProjectListJson getProjects(final Integer offset, final Integer limit) {
-        List<ProjectModel> projects = controller.getProjects(principal, offset, limit);
-        return ProjectListJson.valueOf(projects, offset, controller.countProjects(principal), principal);
-    }
+  @Override
+  @Timed(name = "GetProjectsListTimer", unit = MetricUnits.MILLISECONDS)
+  public ProjectListJson getProjects(final Integer offset, final Integer limit) {
+    List<ProjectModel> projects = controller.getProjects(principal, offset, limit);
+    return ProjectListJson.valueOf(projects, offset, controller.countProjects(principal), principal);
+  }
 
-    @Override
-    @Timed(name = "CreateProjectTimer", unit = MetricUnits.MILLISECONDS)
-    public Response createProject(final ProjectJson project) {
-        final ProjectModel model = controller.createProject(principal, project);
-        final URI location = uri.getAbsolutePathBuilder().path(model.getId().toString()).build();
-        return Response.created(location)
-            .type(MediaType.APPLICATION_JSON)
-            .entity(ProjectJson.valueOf(model))
-            .build();
-    }
+  @Override
+  @Timed(name = "CreateProjectTimer", unit = MetricUnits.MILLISECONDS)
+  public Response createProject(final ProjectJson project) {
+    final ProjectModel model = controller.createProject(principal, project);
+    final URI location = uri.getAbsolutePathBuilder().path(model.getId().toString()).build();
+    return Response.created(location)
+        .type(MediaType.APPLICATION_JSON)
+        .entity(ProjectJson.valueOf(model))
+        .build();
+  }
 
-    @Override
-    @Timed(name = "GetSingleProjectTimer", unit = MetricUnits.MILLISECONDS)
-    public ProjectJson getProject(final UUID projectId) {
-        final ProjectModel model = controller.getProject(principal, projectId);
-        return ProjectJson.valueOf(model);
-    }
+  @Override
+  @Timed(name = "GetSingleProjectTimer", unit = MetricUnits.MILLISECONDS)
+  public ProjectJson getProject(final UUID projectId) {
+    final ProjectModel model = controller.getProject(principal, projectId);
+    return ProjectJson.valueOf(model);
+  }
 
-    @Override
-    @Timed(name = "UpdateProjectTimer", unit = MetricUnits.MILLISECONDS)
-    public ProjectJson updateProject(final UUID projectId, final ProjectJson project) {
-        final ProjectModel model = controller.updateProject(principal, projectId, project);
-        return ProjectJson.valueOf(model);
-    }
+  @Override
+  @Timed(name = "UpdateProjectTimer", unit = MetricUnits.MILLISECONDS)
+  public ProjectJson updateProject(final UUID projectId, final ProjectJson project) {
+    final ProjectModel model = controller.updateProject(principal, projectId, project);
+    return ProjectJson.valueOf(model);
+  }
 
-    @Override
-    @Timed(name = "deleteProjectTimer", unit = MetricUnits.MILLISECONDS)
-    public void deleteProject(final UUID projectId) {
-        controller.deleteProject(principal, projectId);
-    }
+  @Override
+  @Timed(name = "deleteProjectTimer", unit = MetricUnits.MILLISECONDS)
+  public void deleteProject(final UUID projectId) {
+    controller.deleteProject(principal, projectId);
+  }
 
-    @Override
-    public MemberResource getMemberResource() {
-        return resourceContext.initResource(memberResource.get());
-    }
+  @Override
+  public MemberResource getMemberResource() {
+    return resourceContext.initResource(memberResource.get());
+  }
 
-    @Override
-    public PropertyResource getPropertyResource() {
-        return resourceContext.initResource(propertyResource.get());
-    }
+  @Override
+  public PropertyResource getPropertyResource() {
+    return resourceContext.initResource(propertyResource.get());
+  }
 
-    @Override
-    public SiteResource getSiteResource() {
-        return resourceContext.initResource(siteResource.get());
-    }
+  @Override
+  public SiteResource getSiteResource() {
+    return resourceContext.initResource(siteResource.get());
+  }
 
-    @Override
-    public BuildingResource getBuildingResource() {
-        return resourceContext.initResource(buildingResource.get());
-    }
+  @Override
+  public BuildingResource getBuildingResource() {
+    return resourceContext.initResource(buildingResource.get());
+  }
 
-    @Override
-    public ApartmentResource getApartmentResource() {
-        return resourceContext.initResource(apartmentResource.get());
-    }
+  @Override
+  public ApartmentResource getApartmentResource() {
+    return resourceContext.initResource(apartmentResource.get());
+  }
 
-    @Override
-    public CommercialResource getCommercialResource() {
-        return resourceContext.initResource(commercialResource.get());
-    }
+  @Override
+  public CommercialResource getCommercialResource() {
+    return resourceContext.initResource(commercialResource.get());
+  }
 
-    @Override
-    public StorageResource getStorageResource() {
-        return resourceContext.initResource(storageResource.get());
-    }
+  @Override
+  public StorageResource getStorageResource() {
+    return resourceContext.initResource(storageResource.get());
+  }
 
-    @Override
-    public ContractorResource getContractorResource() {
-        return resourceContext.initResource(contractorResource.get());
-    }
+  @Override
+  public ContractorResource getContractorResource() {
+    return resourceContext.initResource(contractorResource.get());
+  }
 
-    @Override
-    public TenancyResource getTenancyResource() {
-        return resourceContext.initResource(tenancyResource.get());
-    }
+  @Override
+  public ProjectTenancyResource getTenancyResource() {
+    return resourceContext.initResource(tenancyResource.get());
+  }
 }
--- remsfal-services/remsfal-platform/src/main/java/de/remsfal/service/control/TenancyController.java
+++ remsfal-services/remsfal-platform/src/main/java/de/remsfal/service/control/TenancyController.java
@@ -3,7 +3,9 @@ package de.remsfal.service.control;
 import de.remsfal.core.model.UserModel;
 import de.remsfal.core.model.project.TenancyModel;
 import de.remsfal.service.entity.dao.TenancyRepository;
+import de.remsfal.service.entity.dao.UserRepository;
 import de.remsfal.service.entity.dto.TenancyEntity;
+import de.remsfal.service.entity.dto.UserEntity;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
@@ -11,7 +13,9 @@ import jakarta.transaction.Transactional.TxType;
 import jakarta.ws.rs.NotFoundException;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import org.jboss.logging.Logger;
 
@@ -26,6 +30,9 @@ public class TenancyController {
 
     @Inject
     TenancyRepository tenancyRepository;
+
+    @Inject
+    UserRepository userRepository;
 
     public List<TenancyEntity> getTenancies(final UserModel tenant) {
         logger.infov("Retrieving all tenancies (tenantId = {0})", tenant.getId());
@@ -58,24 +65,41 @@ public class TenancyController {
         entity.setProjectId(projectId);
         entity.setStartOfRental(tenancy.getStartOfRental());
         entity.setEndOfRental(tenancy.getEndOfRental());
+
+        if (tenancy.getTenants() != null && !tenancy.getTenants().isEmpty()) {
+            List<UserEntity> tenants = tenancy.getTenants().stream()
+                    .map(user -> userRepository.findById(user.getId()))
+                    .filter(Objects::nonNull)
+                    .collect(Collectors.toList());
+            entity.setTenants(tenants);
+        }
+
         tenancyRepository.persistAndFlush(entity);
         return entity;
     }
 
-    @Transactional(TxType.MANDATORY)
-    public TenancyEntity updateTenancy(final UUID projectId, TenancyEntity entity, final TenancyModel tenancy) {
-        if(entity == null) {
-            entity = new TenancyEntity();
-            entity.generateId();
-            entity.setProjectId(projectId);
-        }
-        if(tenancy.getStartOfRental() != null) {
+    @Transactional
+    public TenancyEntity updateTenancy(final UUID projectId, final UUID tenancyId, final TenancyModel tenancy) {
+        logger.infov("Updating a tenancy (projectId={0}, tenancyId={1})", projectId, tenancyId);
+        final TenancyEntity entity = tenancyRepository.findTenancyByProject(projectId, tenancyId)
+                .orElseThrow(() -> new NotFoundException("Tenancy not exist"));
+
+        if (tenancy.getStartOfRental() != null) {
             entity.setStartOfRental(tenancy.getStartOfRental());
         }
-        if(tenancy.getEndOfRental() != null) {
+        if (tenancy.getEndOfRental() != null) {
             entity.setEndOfRental(tenancy.getEndOfRental());
         }
-        return entity;
+
+        if (tenancy.getTenants() != null) {
+            List<UserEntity> tenants = tenancy.getTenants().stream()
+                    .map(user -> userRepository.findById(user.getId()))
+                    .filter(Objects::nonNull)
+                    .collect(Collectors.toList());
+            entity.setTenants(tenants);
+        }
+
+        return tenancyRepository.merge(entity);
     }
 
 }
