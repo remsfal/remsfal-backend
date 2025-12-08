26639b9e26299555956a754ecf8716205a10daae
carlwilliamrix <carlrix1@gmail.com>
2025-12-08 18:04:52 +0100

    Add ProjectTenancyListJson to retrieve all tenancies for a project via ProjectTenancyResource and ProjectTenancyEndpoint and fix path mismatch in ProjectTenancyEndpoint

--- remsfal-core/src/main/java/de/remsfal/core/json/tenancy/TenancyInfoJson.java
+++ remsfal-core/src/main/java/de/remsfal/core/json/tenancy/TenancyInfoJson.java
@@ -20,10 +20,8 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.Null;
 import jakarta.validation.groups.ConvertGroup;
 
-
 import de.remsfal.core.validation.PatchValidation;
 import de.remsfal.core.validation.PostValidation;
-
 
 /**
  * @author Carl Rix [carl.rix@student.htw-berlin.de]
@@ -34,35 +32,35 @@ import de.remsfal.core.validation.PostValidation;
 @Schema(description = "A tenancy item with information from the manager's view")
 @JsonDeserialize(as = ImmutableTenancyInfoJson.class)
 @JsonNaming(PropertyNamingStrategies.LowerCamelCaseStrategy.class)
-public abstract class TenancyInfoJson  implements TenancyModel {
+public abstract class TenancyInfoJson implements TenancyModel {
 
-    @Null(groups = PostValidation.class)
-    @Nullable
-    @Override
-    public abstract UUID getId();
+  @Null(groups = PostValidation.class)
+  @Nullable
+  @Override
+  public abstract UUID getId();
 
-    @Override
-    public abstract LocalDate getStartOfRental();
+  @Override
+  public abstract LocalDate getStartOfRental();
 
-    @Nullable
-    @Override
-    public abstract LocalDate getEndOfRental();
+  @Nullable
+  @Override
+  public abstract LocalDate getEndOfRental();
 
-    @Valid
-    @ConvertGroup(from = PostValidation.class, to = PostValidation.class)
-    @ConvertGroup(from = PatchValidation.class, to = PatchValidation.class)
-    @Nullable
-    @Override
-    public abstract List<UserJson> getTenants();
+  @Valid
+  @ConvertGroup(from = PostValidation.class, to = PostValidation.class)
+  @ConvertGroup(from = PatchValidation.class, to = PatchValidation.class)
+  @Nullable
+  @Override
+  public abstract List<UserJson> getTenants();
 
-    public static TenancyInfoJson valueOf(TenancyModel model) {
-        return ImmutableTenancyInfoJson.builder()
-                .id(model.getId())
-                .startOfRental(model.getStartOfRental())
-                .endOfRental(model.getEndOfRental())
-                .tenants(model.getTenants().stream()
-                        .map(UserJson::valueOf)
-                        .toList())
-                .build();
-    }
+  public static TenancyInfoJson valueOf(TenancyModel model) {
+    return ImmutableTenancyInfoJson.builder()
+        .id(model.getId())
+        .startOfRental(model.getStartOfRental())
+        .endOfRental(model.getEndOfRental())
+        .tenants(model.getTenants() != null ? model.getTenants().stream()
+            .map(UserJson::valueOf)
+            .toList() : null)
+        .build();
+  }
 }
