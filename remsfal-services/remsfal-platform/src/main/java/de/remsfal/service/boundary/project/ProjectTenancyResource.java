8e27531ed3564f0b30aee5e1b260a2c3bd3ed8fe
Alexander Stanik <alexander.stanik@htw-berlin.de>
2026-01-10 11:29:04 +0100

    Don't use entities in boundary

--- remsfal-services/remsfal-platform/src/main/java/de/remsfal/service/boundary/project/ProjectTenancyResource.java
+++ remsfal-services/remsfal-platform/src/main/java/de/remsfal/service/boundary/project/ProjectTenancyResource.java
@@ -3,16 +3,16 @@ package de.remsfal.service.boundary.project;
 import java.net.URI;
 import java.util.UUID;
 
-import de.remsfal.core.api.project.ProjectTenancyEndpoint;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
+import de.remsfal.core.api.project.ProjectTenancyEndpoint;
 import de.remsfal.core.json.tenancy.ProjectTenancyListJson;
 import de.remsfal.core.json.tenancy.TenancyInfoJson;
+import de.remsfal.core.model.project.TenancyModel;
 import de.remsfal.service.control.TenancyController;
-import de.remsfal.service.entity.dto.TenancyEntity;
 
 import java.util.List;
 
@@ -25,21 +25,21 @@ public class ProjectTenancyResource extends ProjectSubResource implements Projec
     @Override
     public TenancyInfoJson getTenancy(final UUID projectId, final UUID tenancyId) {
         checkReadPermissions(projectId);
-        final TenancyEntity tenancy = tenancyController.getTenancyByProject(projectId, tenancyId);
+        final TenancyModel tenancy = tenancyController.getTenancyByProject(projectId, tenancyId);
         return TenancyInfoJson.valueOf(tenancy);
     }
 
     @Override
     public ProjectTenancyListJson getTenancies(final UUID projectId) {
         checkReadPermissions(projectId);
-        final List<TenancyEntity> tenancies = tenancyController.getTenanciesByProject(projectId);
+        final List<? extends TenancyModel> tenancies = tenancyController.getTenanciesByProject(projectId);
         return ProjectTenancyListJson.valueOf(tenancies);
     }
 
     @Override
     public Response createTenancy(final UUID projectId, final TenancyInfoJson tenancy) {
         checkWritePermissions(projectId);
-        final TenancyEntity entity = tenancyController.createTenancy(projectId, tenancy);
+        final TenancyModel entity = tenancyController.createTenancy(projectId, tenancy);
         final URI location = uri.getAbsolutePathBuilder().path(String.valueOf(entity.getId())).build();
         return Response.created(location)
             .type(MediaType.APPLICATION_JSON)
@@ -50,7 +50,7 @@ public class ProjectTenancyResource extends ProjectSubResource implements Projec
     @Override
     public TenancyInfoJson updateTenancy(final UUID projectId, final UUID tenancyId, final TenancyInfoJson tenancy) {
         checkWritePermissions(projectId);
-        final TenancyEntity entity = tenancyController.updateTenancy(projectId, tenancyId, tenancy);
+        final TenancyModel entity = tenancyController.updateTenancy(projectId, tenancyId, tenancy);
         return TenancyInfoJson.valueOf(entity);
     }
 
