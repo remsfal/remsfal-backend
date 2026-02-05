762b835ffbf570648323f5fb3b7380b0aa881fd2
Alexander Stanik <alexander.stanik@htw-berlin.de>
2026-02-05 14:41:22 +0100

    checkstyle

--- remsfal-core/src/main/java/de/remsfal/core/validation/AtLeastOneRentUnitValidator.java
+++ remsfal-core/src/main/java/de/remsfal/core/validation/AtLeastOneRentUnitValidator.java
@@ -5,9 +5,8 @@ import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 /**
- * Validator implementation for {@link AtLeastOneRentUnit} annotation.
- * Checks that at least one rent unit list (apartmentRents, buildingRents, propertyRents,
- * siteRents, storageRents, commercialRents) is not empty.
+ * Validator implementation for {@link AtLeastOneRentUnit} annotation. Checks that at least one rent unit list
+ * (apartmentRents, buildingRents, propertyRents, siteRents, storageRents, commercialRents) is not empty.
  *
  * @author Alexander Stanik [alexander.stanik@htw-berlin.de]
  */
@@ -20,17 +19,18 @@ public class AtLeastOneRentUnitValidator implements ConstraintValidator<AtLeastO
         }
 
         boolean hasAtLeastOneRent = hasElements(value.getApartmentRents())
-                || hasElements(value.getBuildingRents())
-                || hasElements(value.getPropertyRents())
-                || hasElements(value.getSiteRents())
-                || hasElements(value.getStorageRents())
-                || hasElements(value.getCommercialRents());
+            || hasElements(value.getBuildingRents())
+            || hasElements(value.getPropertyRents())
+            || hasElements(value.getSiteRents())
+            || hasElements(value.getStorageRents())
+            || hasElements(value.getCommercialRents());
 
         if (!hasAtLeastOneRent) {
             context.disableDefaultConstraintViolation();
             context.buildConstraintViolationWithTemplate(
-                    "At least one rent unit (apartmentRents, buildingRents, propertyRents, siteRents, storageRents, or commercialRents) must be specified")
-                    .addConstraintViolation();
+                "At least one rent unit (apartmentRents, buildingRents, propertyRents, "
+                + "siteRents, storageRents, or commercialRents) must be specified")
+                .addConstraintViolation();
             return false;
         }
 
