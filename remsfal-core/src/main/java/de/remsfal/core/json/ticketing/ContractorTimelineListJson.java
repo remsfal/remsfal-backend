bc2f67e0b6ae2769fbd9d2425ea16c009ce798bc
DDreat <153623020+DDreat@users.noreply.github.com>
2026-09-09 12:43:21 +0200

    CanMessageTenant wird mit ins Frontend gegeben, wenn die Contractortimeline aufgerufen wird. (#873)

--- remsfal-core/src/main/java/de/remsfal/core/json/ticketing/ContractorTimelineListJson.java
+++ remsfal-core/src/main/java/de/remsfal/core/json/ticketing/ContractorTimelineListJson.java
@@ -23,11 +23,20 @@ public abstract class ContractorTimelineListJson {
     @Schema(description = "Timeline entries", readOnly = true)
     public abstract List<ContractorTimelineJson> getTimelines();
 
+    @Schema(description = "Whether a new timeline entry can be sent to the tenant", readOnly = true)
+    public abstract boolean isVisibleToTenant();
+
     public static ContractorTimelineListJson valueOf(final List<? extends ContractorTimelineModel> timelines) {
+        return valueOf(timelines, false);
+    }
+
+    public static ContractorTimelineListJson valueOf(final List<? extends ContractorTimelineModel> timelines,
+        final boolean visibleToTenant) {
         return ImmutableContractorTimelineListJson.builder()
             .timelines(timelines.stream()
                 .map(ContractorTimelineJson::valueOf)
                 .toList())
+            .visibleToTenant(visibleToTenant)
             .build();
     }
 
