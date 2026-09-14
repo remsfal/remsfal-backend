e7685c47d4ffec09457d0c1df533c1db12828955
Philipp Bogatchev <philipp.bogatchev@remsfal.online>
2026-09-14 13:27:40 +0200

    checkstyle

--- remsfal-core/src/main/java/de/remsfal/core/api/ticketing/contractor/ContractorIssueRequestEndpoint.java
+++ remsfal-core/src/main/java/de/remsfal/core/api/ticketing/contractor/ContractorIssueRequestEndpoint.java
@@ -9,7 +9,6 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
 
 import java.util.UUID;
 
