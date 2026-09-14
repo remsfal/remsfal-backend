de7cd82b9b8fd03b96a7cf02f7d7259f579e7e99
Philipp Bogatchev <philipp.bogatchev@remsfal.online>
2026-09-14 09:25:27 +0200

    sonar

--- remsfal-core/src/test/java/de/remsfal/core/json/ticketing/IssueRequestJsonTest.java
+++ remsfal-core/src/test/java/de/remsfal/core/json/ticketing/IssueRequestJsonTest.java
@@ -8,7 +8,6 @@ import java.util.List;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
 
 class IssueRequestJsonTest {
 
